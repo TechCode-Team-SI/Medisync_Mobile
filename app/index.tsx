2c56966b9111da2f973aff8b035c87d11826fd1d
yaiscg <yaisraylith@gmail.com>
2024-08-26 22:17:59 -0400

    .

--- app/index.tsx
+++ app/index.tsx
@@ -2,9 +2,6 @@ import { View } from "react-native";
 import styles from "@/src/components/LoginComponents/stylesLogin";
 
 import LoginPage from "@/src/pages/Login/LoginPage";
-import HomePage from "@/src/pages/HomePage";
-import Prueba from "@/src/pages/Prueba";
-import RegisterPage from "@/src/pages/Login/RegisterPage";
 
 export default function HomeScreen() {
   return (
