2b4f3219ea140c2062c3f0be30d26b075621af51
yaiscg <yaisraylith@gmail.com>
2024-08-26 23:50:47 -0400

    .

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -9,9 +9,9 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
-    Montserrat: require("../assets/fonts/Montserrat-Bold.ttf"),
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-    Roboto: require("../assets/fonts/Roboto-Regular.ttf"),
+    Montserrat: require("../../assets/fonts/Montserrat-Bold.ttf"),
+    SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
+    Roboto: require("../../assets/fonts/Roboto-Regular.ttf"),
   });
 
   useEffect(() => {
