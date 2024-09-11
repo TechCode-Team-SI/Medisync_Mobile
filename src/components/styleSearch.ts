a3ab6fdfd47d112d200e42ec692549e040ea3ff5
Jesymar Lopez <86635217+jesymarlop@users.noreply.github.com>
2024-09-11 14:09:25 -0400

    Pagina de buscar especialidad y buscar doctor.

---
+++ src/components/styleSearch.ts
@@ -0,0 +1,25 @@
+const stylesSearch = {
+    container1: 'flex-1 bg-primary relative', 
+     //// Background 
+     containerBg1: 'absolute bottom-0 w-full h-4/6 bg-principal rounded-t-3xl pt-8 px-4',
+     containerBg2: 'absolute items-center bottom-0 w-full h-4/6 bg-principal rounded-t-3xl pt-8 px-6',
+
+     title1: "text-principal font-montserrat text-center pt-12 text-3xl px-22 m-4" ,
+     title2: " font-montserrat text-left my-3 text-primary text-xl",   
+     title3: " font-montserrat text-left  text-primary text-l",  
+
+     //Button back
+    buttonBack1: 'w-full h-12 items-center justify-center bg-secondary rounded-lg', 
+
+    ///Button
+    containerGrid:"flex-row flex-wrap gap-4 space-x-3",
+    button: 'basis-[28.5714286%] w-11/12 h-24 items-center justify-center bg-terciary rounded-xl ', 
+    buttonText: ' mt-2 h-7 text-primary text-m font-montserrat',
+
+    container:"flex-col gap-4",
+    button2: 'shadow flex-row w-full space-x-4 h-20 items-center justify-center bg-terciary rounded-xl ', 
+    buttonText2: 'basis-3/4 mt-2 h-7 text-primary text-m font-montserrat',
+    
+};
+    
+export default stylesSearch;
