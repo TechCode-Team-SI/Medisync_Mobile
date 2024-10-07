11e1bd93640890b73b14dda1943f0ae749b598bc
yaiscg <yaisraylith@gmail.com>
2024-10-06 20:52:07 -0400

    fix support chat page

--- src/components/SupportComponents/stylesChat.ts
+++ src/components/SupportComponents/stylesChat.ts
@@ -1,34 +1,45 @@
 const stylesChat = {
-    /////General////
+    /////General
     container: 'flex-1 bg-principal relative', 
-    
-    container6: "bg-primaryDisable flex-row mb-6 p-2",
-    container7: "bg-gray-300 border-gray-300 h-45 borderRadius-2 m-3 rounded-lg p-3 mt-1",
-    container13: "bg-gray-300 border-gray-300 h-16 -m-1 p-3 ml-1 -mt-1 ",
-    container8: "bg-principal  flex-row left-1  -mt-3 ",
-    container9: "bg-terciary h-16 borderRadius-2 m-1 rounded-lg p-5 left-9 -mt-7 w-80 ml-2",
-    container15: "bg-terciary h-11  m-2 p-1 mt-5 -ml-3 ",
-    container10: "bg-withe h-3/8 borderRadius-2 m-0 rounded-lg p-3 -mt-5 w-full",
-    container11: " bg-gray-100 border-black-300 h-12 borderRadius-2 m-5 ml-2 mt-1 rounded-lg -p-1 ",
-    container12: "flex flex-reverse   left-80  mt-3 ml-6",
-    container16:"bg-Principal border-y-4 border-principal w-fll h-40 w-full",
 
-     ////Titulo del primer Cuadro de information
-    title1:"text-black text-xl  ml-12 -mt-9 left-11 font-bold ",
-    title2:"text-secondary text-xl ml-2 mt-1 left-0 font-bold ",
-    title8:"text-black text-1x3  left-10 -ml-11 -mt-3 font-bold ",
-    ///Titulo del Topbar
-    title3:"text-secondary text-xl ml-8 mt-8",
-    ///Titulo del boton de Respuestas
-    title4:"text-secondary text-2 -ml-1 mt-2",
-    ///Fecha y hora 
-    title5:"text-secondary text-1 -mb-4 -mt-5 left-40 -ml-8",
-    title9:"text-secondary text-1 -mb-6 -mt-1 left-60 -ml-3",
-    ///Titulo de Agente de soporte, Nombre de usaurio y Informcaion
-    title6:"text-black text-1x3 ml-11 mt-4 left-2 font-bold ",
-    title10:"text-primary text-1 -mb-6 -mt-1 ml-1 font-bold",
-    ///Imput 
-    input: ' mx-1 w-64 h-full -mt-9 h-10 ml-3',
+    ///// Info del ticket (superior)
+
+    container1: "bg-gray-200 h-36  m-3 rounded-tr-xl mx-4 mt-8 rounded-tr-2xl rounded-bl-2xl rounded-br-2xl",
+    containerIcon:"flex-row my-4 ml-4",
+    containerInfo:" mx-4 mb-2",
+
+    title:"font-bold text-lg pl-4",
+    title2:"font-bold text-primary text-base ",
+    text:"pt-1",
+
+    /////Input (inferior)
+
+    container2:"flex-1 justify-end mb-6",
+    containerInput: "flex-row bg-gray-200 h-12 rounded-lg  mx-4 items-center",
+    input: ' flex-1 pl-4',
+    iconButton: "pr-2",
+
+    ////Bloque de mensajes
+    container3:"flex-grow mb-4",
+    
+    container4:"mr-11 ml-4",
+    containerInfo2: "bg-terciary h-16 m-1 rounded-tr-2xl rounded-bl-2xl rounded-br-2xl left-7 -mt-7 w-auto ",
+
+    containerDate:"flex-row justify-end mr-4 mt-2",
+    containerMessage:"mx-4",
+
+    //// texto
+
+        title4:"text-black text-1x3 ml-9 mt-4 font-bold ", ///titulo de Usuario o Soporte
+        textDate:"text-secondary mx-2", //fecha
+        textHour:"text-secondary ", //hora
+        textMessage:"text-primary", //mensaje
+
+    //// Botón de respuestas
+    container5: "flex-row items-center ml-4",
+    title3:"text-primary text-2 ml-1",
+
+
     };
     
     export default stylesChat;
--- src/pages/Support/SupportChatPage.tsx
+++ src/pages/Support/SupportChatPage.tsx
@@ -1,138 +1,89 @@
-//// ARREGLAR //////
-
 import React, { useState } from 'react';
-import { View, TextInput, Text, TouchableOpacity,ScrollView } from 'react-native';
+import { View, TextInput, Text, TouchableOpacity, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import styles from '@/src/components/SupportComponents/stylesChat';
-import AntDesign from '@expo/vector-icons/AntDesign';
 import Entypo from '@expo/vector-icons/Entypo';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import Octicons from '@expo/vector-icons/Octicons';
-import { Link } from 'expo-router'; 
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import TopBarSupport from '@/src/components/SupportComponents/TopBarSupport';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const SupportChatPage: React.FC = () => {
-
-
   const [inputTex, setInputTex] = useState('');
 
   return (
-    <View className={styles.container}>
-           <View className={styles.container6}>
-          <TouchableOpacity onPress={() => alert("")} className="p-3 mt-5">
-                  {<AntDesign name="arrowleft" size={30} color="#68C3B7" className="p-2"/>}
-         </TouchableOpacity>
-         <Text  className={styles.title3}> Agente de Soporte
-         </Text>
-         </View>
-        <View className={styles.container7}>
-        <FontAwesome name="user-md" size={70} color="#68C3B7" className="p-4"/>
-            <Text className={styles.title1}>
-                Nombre de Usuario
-            </Text>
-            <Text className={styles.title2}>
-                Title
-            </Text>
-            <View className={styles.container13}>
-            <Text className={styles.title8}>
-                Information
-            </Text>
+    <KeyboardAvoidingView 
+      style={{ flex: 1 }} 
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 80 : 0}
+    >
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View className={styles.container}>
+          <TopBarSupport title="Agente de Soporte" />
+
+          <View className={styles.container1}>
+            <View className={styles.containerIcon}>
+              <FontAwesome name="user-circle" size={33} color="#539091" />
+              <Text className={styles.title}>Nombre de Usuario</Text>
             </View>
-        </View>
-        <View className={styles.container8}>
-          <TouchableOpacity onPress={() => alert("")} className="p-2">
-                  {<Entypo name="chevron-down" size={25} color="#68C3B7" />}
-         </TouchableOpacity>
-              <Text className={styles.title4}>
-                Respuesta
-               </Text>
-        </View>
-        <View className={styles.container10}>
-             <Text className={styles.title6}>
-                 Agente de Soporte
-               </Text>
-            {<FontAwesome name="user-md" size={35} color="#68C3B7"/>}
-          <View className={styles.container9}>
-              <Text className={styles.title5}>
-                 dd/mm/aaaa
-               </Text>
-               <Text className={styles.title9}>
-                 hh:hh am
-               </Text>
-               <View className={styles.container15}>
-               <Text className={styles.title10}>
-                 information
-               </Text>
-               </View>
+
+            <View className={styles.containerInfo}>
+              <Text className={styles.title2}>Título</Text>
+              <Text className={styles.text}>Información del ticket.</Text>
+            </View>
           </View>
 
-             <Text className={styles.title6}>
-             Nombre de Usuario
-               </Text>
-           {<FontAwesome name="user-circle" size={33} color="#68C3B7"/>}
-          <View className={styles.container9}>
-              <Text className={styles.title5}>
-              dd/mm/aaaa
-               </Text>
-               <Text className={styles.title9}>
-                 hh:hh am
-               </Text>
-               <View className={styles.container15}>
-               <Text className={styles.title10}>
-                 information
-               </Text>
-               </View>
-          </View>
-             
-             <Text className={styles.title6}>
-               Agente de Soporte
-               </Text>
-            {<FontAwesome name="user-md" size={35} color="#68C3B7" />}
-          <View className={styles.container9}>
-              <Text className={styles.title5}>
-              dd/mm/aaaa
-               </Text>
-               <Text className={styles.title9}>
-                 hh:hh am
-               </Text>
-               <View className={styles.container15}>
-               <Text className={styles.title10}>
-                 information
-               </Text>
-               </View>
-          </View>
-             
-             <Text className={styles.title6}>
-             Nombre de Usuario
-               </Text>
-            {<FontAwesome name="user-circle" size={33} color="#68C3B7"/>}
-          <View className={styles.container9}>
-              <Text className={styles.title5}>
-              dd/mm/aaaa
-               </Text>
-               <Text className={styles.title9}>
-                 hh:hh am
-               </Text>
-               <View className={styles.container15}>
-               <Text className={styles.title10}>
-                 information
-               </Text>
-               </View>
-          </View>  
-        </View>
+          <TouchableOpacity className={styles.container5} onPress={() => alert("")}>
+            <Entypo name="chevron-down" size={25} color="#539091" />
+            <Text className={styles.title3}>Respuestas</Text>
+          </TouchableOpacity>
 
-        <View className={styles.container11}>
-        <TouchableOpacity onPress={() => alert("")} className={styles.container12}>
-        <Octicons name="paper-airplane" size={26} color="#68C3B7" />
-        </TouchableOpacity>
-        <TextInput
-          className={styles.input}
-          placeholder="Escribe tu Mensaje"
-          placeholderTextColor="#539091"
-          value={inputTex}
-          onChangeText={setInputTex}
-          />
-        </View>
+          <View className={styles.container3}>
+            <View className={styles.container4}>
+              <Text className={styles.title4}>Agente de Soporte</Text>
+              <FontAwesome5 name="info-circle" size={30} color="#539091" />
+              <View className={styles.containerInfo2}>
+                <View className={styles.containerDate}>
+                  <Text className={styles.textDate}>dd/mm/aaaa</Text>
+                  <Text className={styles.textHour}>hh:hh am</Text>
+                </View>
+                <View className={styles.containerMessage}>
+                  <Text className={styles.textMessage}>Mensaje</Text>
+                </View>
+              </View>
+            </View>
 
-    </View>
+            <View className={styles.container4}>
+              <Text className={styles.title4}>Nombre de usuario</Text>
+              <FontAwesome name="user-circle" size={30} color="#539091" />
+              <View className={styles.containerInfo2}>
+                <View className={styles.containerDate}>
+                  <Text className={styles.textDate}>dd/mm/aaaa</Text>
+                  <Text className={styles.textHour}>hh:hh am</Text>
+                </View>
+                <View className={styles.containerMessage}>
+                  <Text className={styles.textMessage}>Mensaje</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+
+          <View className={styles.container2}>
+            <View className={styles.containerInput}>
+              <TextInput
+                className={styles.input}
+                placeholder="Escribe tu mensaje"
+                placeholderTextColor="#539091"
+                value={inputTex}
+                onChangeText={setInputTex}
+              />
+              <TouchableOpacity onPress={() => alert("")} className={styles.iconButton}>
+                <Ionicons name="send-sharp" size={28} color="#539091" />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
