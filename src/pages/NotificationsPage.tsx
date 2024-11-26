d4ed06c46daf0c8624cf5dc14264faa43219ba7a
yaiscg <yaisraylith@gmail.com>
2024-11-26 15:17:04 -0400

    feat: add functionality to mark all notifications as read

--- src/pages/NotificationsPage.tsx
+++ src/pages/NotificationsPage.tsx
@@ -1,16 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { Text, View, FlatList } from "react-native";
-
+import React, { useState } from "react";
+import { Text, View, FlatList, Button, TouchableOpacity } from "react-native";
 import TopBar from "../components/Navigation/TopBar";
 import SideMenuModal from "../components/Navigation/SideMenuModal";
 import { useFocusEffect } from '@react-navigation/native';
-import { getNotifications } from "../services/user/notificationsServices";
+import { getNotifications, markAllAsRead } from "../services/user/notificationsServices";
 import Loader from "../components/ui/Loader";
 
 const NotificationsPage: React.FC = () => {
   const [isMenuVisible, setMenuVisible] = useState(false);
   const [notifications, setNotifications] = useState<any[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);  // Estado de carga
+  const [isLoading, setIsLoading] = useState<boolean>(true);  
 
   const toggleMenu = () => {
     setMenuVisible(prev => !prev);
@@ -19,37 +18,54 @@ const NotificationsPage: React.FC = () => {
   useFocusEffect(
     React.useCallback(() => {
       const fetchNotifications = async () => {
-        setIsLoading(true);  // Activamos el estado de carga
+        setIsLoading(true);  
         const result = await getNotifications();
-        console.log("Resultado de la API de notificaciones:", result); 
+        console.log(result.data.data)
         if (result.success) {
-          console.log("Notificaciones:", result.data.data); 
           setNotifications(result.data.data); 
-        } else {
-          console.log("Error al obtener notificaciones:", result.message); 
         }
-        setIsLoading(false);  // Desactivamos el estado de carga
+        setIsLoading(false);  
       };
-
       fetchNotifications(); 
     }, [])
   );
 
+  const markAllAsReadHandler = async () => {
+    const result = await markAllAsRead();
+    if (result.success) {
+      setNotifications(prevNotifications => 
+        prevNotifications.map(notification => ({
+          ...notification,
+          read: true 
+        }))
+      );
+    }
+  };
+
   const renderNotification = ({ item }: { item: any }) => (
-    <View className="p-4 mb-2 bg-white rounded-lg shadow-md">
+    <View className={`p-4 mb-2 rounded-lg shadow-md ${item.read ? 'bg-white' : 'bg-bgInput'}`}>
       <Text className="text-lg text-primary font-montserrat">{item.title}</Text>
-      <Text className="text-sm text-gray-400 py-2">{item.content}</Text>
+      <Text className="text-sm text-gray-500 py-2">{item.content}</Text>
     </View>
   );
 
   return (
     <View className="flex-1 bg-gray-100">
       <TopBar title="Notificaciones" onLeftPress={toggleMenu} />
-
       <SideMenuModal isVisible={isMenuVisible} onClose={() => setMenuVisible(false)} />
 
       <View className="flex-1 p-4">
-        {isLoading ? (  // Si está cargando, muestra el Loader
+
+        <View className="mt-2 mb-4 items-end">
+          <TouchableOpacity 
+              onPress={markAllAsReadHandler} 
+              className="bg-secondary p-2.5 rounded-3xl shadow-md h-9"
+            >
+              <Text className="text-white text-center text-xs font-bold">Marcar como leídas</Text>
+            </TouchableOpacity>
+        </View>
+
+        {isLoading ? (
           <Loader />
         ) : notifications.length === 0 ? (
           <Text className="text-center text-gray-400 py-6 text-lg font-bold">No tienes notificaciones</Text>
@@ -66,4 +82,3 @@ const NotificationsPage: React.FC = () => {
 };
 
 export default NotificationsPage;
-
--- src/services/user/notificationsServices.ts
+++ src/services/user/notificationsServices.ts
@@ -2,8 +2,6 @@ import axios from 'axios';
 import { getToken } from '../auth/sessionServices'; 
 import { api } from '../api/apiConfig';
 
-
-
 export const getNotifications = async () => {
   const token = await getToken(); 
   
@@ -23,4 +21,28 @@ export const getNotifications = async () => {
   } catch (error: any) {
     console.log("Error al obtener las notificaciones:", error);
     return { success: false, message: "Error al obtener las notificaciones." };
-}};
+  }
+};
+
+
+export const markAllAsRead = async () => {
+  const token = await getToken(); 
+
+  if (!token) {
+    console.log("No se encontró el token de autenticación.");
+    return { success: false, message: "No se encontró el token de autenticación." };
+  }
+
+  try {
+    const response = await axios.post(api.notificationsReadAll, {}, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return { success: response.data.success }; 
+  } catch (error: any) {
+    console.log("Error al marcar todas las notificaciones como leídas:", error);
+    return { success: false, message: "Error al marcar todas las notificaciones como leídas." };
+  }
+};
